16092446be37f054b847532624c24f4bc4010908
Brett Ezell <dnbeze55@gmail.com>
2024-02-22 10:16:13 -0500

    latest update

--- models/user.go
+++ models/user.go
@@ -4,24 +4,23 @@ import "ginmx2/db"
 
 type User struct {
 	ID        int64
-	Username  string `binding:"required"`
+	Email     string `binding:"required"`
 	Password  string `binding:"required"`
-	email     string `binding:"required"`
-	firstname string `binding:"required"`
-	lastname  string `binding:"required"`
+	Firstname string `binding:"required"`
+	Lastname  string `binding:"required"`
 }
 
 func (u User) Save() error {
 	query := `
-	INSERT INTO users (username, password, email, firstname, lastname) 
-	VALUES (?, ?, ?, ?, ?)`
+	INSERT INTO users (email, password, firstname, lastname) 
+	VALUES (?, ?, ?, ?)`
 
 	data, err := db.DB.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer data.Close()
-	result, err := data.Exec(u.Username, u.Password, u.email, u.firstname, u.lastname)
+	result, err := data.Exec(u.Email, u.Password, u.Firstname, u.Lastname)
 	if err != nil {
 		return err
 	}
