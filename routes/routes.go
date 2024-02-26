bbabcc65d60da955cef084138663e0442109a7b0
Brett Ezell <dnbeze55@gmail.com>
2024-02-26 00:30:39 -0500

    modified middleware implementation to server group

--- routes/routes.go
+++ routes/routes.go
@@ -8,10 +8,14 @@ import (
 func BuildRoutes(server *gin.Engine) {
 	server.GET("/ping", getPong) // GET POST PUT PATCH DELETE OPTIONS HEAD
 	server.GET("/notes", getNotes)
-	server.GET("/notes/:id", getNote)                          // dynamic route
-	server.POST("/notes", middleware.Authenticate, createNote) // NEW entry // protected // can register multiple request handlers for middleware
-	server.PUT("/notes/:id", updateNote)                       // UPDATE entry
-	server.DELETE("/notes/:id", deleteNote)                    // DELETE entry
-	server.POST("/signup", signup)                             // new user
-	server.POST("/login", login)                               // login
+	server.GET("/notes/:id", getNote) // dynamic route
+
+	authenticated := server.Group("/")
+	authenticated.Use(middleware.Authenticate)     // middleware defined for group
+	authenticated.POST("/notes", createNote)       // NEW entry // protected // can register multiple request handlers for middleware
+	authenticated.PUT("/notes/:id", updateNote)    // UPDATE entry
+	authenticated.DELETE("/notes/:id", deleteNote) // DELETE entry
+
+	server.POST("/signup", signup) // new user
+	server.POST("/login", login)   // login
 }
